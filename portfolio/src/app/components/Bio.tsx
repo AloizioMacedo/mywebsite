bf4791b11f4a57e5e60ae39882b54e82def767c4
AloizioMacedo <atsam2@hotmail.com>
2024-03-30 14:56:27 -0300

    Restyle Bio.tsx

--- portfolio/src/app/components/Bio.tsx
+++ portfolio/src/app/components/Bio.tsx
@@ -1,6 +1,67 @@
+import styles from "./bio.module.css";
+
 export function Bio() {
     return (
-        <div className="bio">
+        <div className={styles.bioText}>
+            <Description />
+            <Technologies />
+        </div>
+    );
+}
+
+function Description() {
+    return (
+        <div className={styles.description}>
+            <p>
+                Senior SWE currently leading asset teams that develop Data
+                Science solutions.
+            </p>
+            <p>
+                My background as a PhD in Mathematics enables me to quickly
+                adapt to different problems and contexts.
+                <br /> Here are some specific examples of business use cases
+                that I&apos;ve been directly involved with:
+            </p>
+            <ul className={styles.useCases}>
+                <li>Traffic Control</li>
+                <li>Workforce Scheduling </li>
+                <li>Routing algorithms </li>
+            </ul>
+            <p>
+                I lead by doing and am not very fond of contributing in a
+                hands-off management style. As such, I am inextricably involved
+                in the development process, and I firmly believe that this helps
+                to build a team that trusts the solution, the development
+                process and the leadership. I am familiar with a plethora of
+                technologies, including:
+            </p>
+
+            <p>
+                Furthermore, the teams I&apos;ve led consist of very different
+                archetypes, including
+            </p>
+            <ul>
+                <li>Software Engineers</li>
+                <li>Data Scientists</li>
+                <li>Data Engineers</li>
+                <li>Machine Learning Engineers</li>
+                <li>Mathematicians</li>
+                <li>Data Analysts</li>
+            </ul>
+            <p>
+                These days, most often I contribute with Data Scientists in
+                assetizing different implementations developed in specific
+                client contexts, making sure to create reusable, efficient and
+                easy-to-use solutions. You can take a look at some of my
+                side-projects at my GitHub: https://github.com/AloizioMacedo.
+            </p>
+        </div>
+    );
+}
+
+function OldDescription() {
+    return (
+        <div className={styles.description}>
             <p>
                 Senior SWE currently leading asset teams that develop Data
                 Science solutions.
@@ -46,17 +107,7 @@ export function Bio() {
                 process and the leadership. I am familiar with a plethora of
                 technologies, including:
             </p>
-            <ul>
-                <li>Python</li>
-                <li>Rust</li>
-                <li>Git</li>
-                <li>Docker</li>
-                <li>Go</li>
-                <li>Typescript</li>
-                <li>SQL and NoSQL databases</li>
-                <li>AWS</li>
-                <li>Azure</li>
-            </ul>
+
             <p>
                 Furthermore, the teams I&apos;ve led consist of very different
                 archetypes, including
@@ -76,6 +127,25 @@ export function Bio() {
                 easy-to-use solutions. You can take a look at some of my
                 side-projects at my GitHub: https://github.com/AloizioMacedo.
             </p>
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
+        <div>
+            <p>Technologies:</p>
+            <ul className={styles.technologies}>
+                <li>Python</li>
+                <li>Rust</li>
+                <li>Git</li>
+                <li>Docker</li>
+                <li>Go</li>
+                <li>Typescript</li>
+                <li>SQL/NoSQL databases</li>
+                <li>AWS</li>
+                <li>Azure</li>
+            </ul>
         </div>
     );
 }
